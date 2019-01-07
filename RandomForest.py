300861a3f24676bd29a997231279cf58f7f4fbf3
John Hebeler <jhebeler@gmail.com>
2019-01-06 22:17:47 -0500

    Random Forest working

--- RandomForest.py
+++ RandomForest.py
@@ -7,6 +7,10 @@ import pandas as pd
 X = np.load('data/MLindependent.npy')
 
 y = np.load('data/MLdependent.npy')
+#y = y.tolist()
+
+y = y.reshape(58,)
+y = y.astype(int)
 
 XNames = np.load('data/MLindependentNames.npy')
 yNames = np.load('data/MLdependentNames.npy')
@@ -18,11 +22,49 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, rand
 # Fitting classifier to the Training set
 from sklearn.ensemble import RandomForestClassifier
 classifier = RandomForestClassifier(n_estimators = 10,criterion = 'entropy', random_state = 0)
+#classifier.fit(X_train, np.ravel(y_train))
 classifier.fit(X_train, y_train)
-
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Random Forest (Training set)')
+plt.xlabel(XNames[0])
+plt.ylabel(XNames[1])
+plt.legend()
+plt.show()
+
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Random Forest (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
